cc724df9e1ac601294e4cd9150a8efcb33199323
Melissa Real <melissa.real@hotmail.com>
2018-12-06 15:48:00 +0000

    Automatically backed up by Learn

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,19 @@
+class GenresController < ApplicationController
+
+  get '/genres' do
+    @genres = Genre.all
+    erb :'/genres/index'
+  end
+
+  get '/genres/:id' do
+    @genre = Genre.find_by_id(params[:id])
+    erb :'/genre/show'
+  end
+
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'/genre/show'
+  end
+
+
+end
