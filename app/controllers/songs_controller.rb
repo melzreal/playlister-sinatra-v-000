97bfc1faa757ed66d088a322ada24fec0218e1e2
Melissa Real <melissa.real@hotmail.com>
2018-12-08 11:29:44 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,8 +10,24 @@ class SongsController < ApplicationController
    end
 
    post '/songs' do
-     redirect to "songs/#{@song.id}"
+     @song = Song.create(:name => params["Name"])
+     if !params["Artist Name"].empty?
+       @new_artist = Artist.find_or_create_by(:name => params["Artist Name"])
+       @song.artist = @new_artist
+     end
+     @song.genre_ids = params["genres"]
+     @song.save
+     flash[:message] = "Successfully created song."
+
+     redirect "/songs/#{@song.slug}"
+
    end
+
+   get '/songs/:slug/edit' do
+     @song = Song.find_by_slug(params[:slug])
+     erb :'/songs/edit'
+   end
+
 
    get '/songs/:slug' do
      @song = Song.find_by_slug(params[:slug])
@@ -19,20 +35,20 @@ class SongsController < ApplicationController
      erb :'/songs/show'
    end
 
-   patch '/songs/:id' do
-     @song = Song.find(params[:id])
-     @song.update(params["song"])
-     if !params["artist"]["name"].empty?
-       @song.artist = artist.create(name: params["artist"]["name"])
-     end
+
+
+   patch '/songs/:slug' do
+     @song = Song.find_by_slug(params[:slug])
+     @song.update(params[:song])
+     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+     @song.genre_ids = params[:genres]
      @song.save
-     redirect "songs/#{@song.id}"
+
+     flash[:message] = "Successfully updated song."
+
+     redirect "/songs/#{@song.slug}"
    end
 
-   get '/songs/:id/edit' do
-     @song = Song.find_by_id(params[:id])
-     erb :'/songs/edit'
-   end
 
 
 end
